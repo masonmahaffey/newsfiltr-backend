5b9602448547aaa1acf56f21af22e5e0ccc0680f
Mason Mahaffey <masonmahaffey00@gmail.com>
2019-07-09 01:14:31 -0400

    Update config.js

--- js/common/config.js
+++ js/common/config.js
@@ -3,8 +3,8 @@
  */
 module.exports = {
     EMAIL_SERVICE   : process.env.EMAIL_SERVICE           || 'gmail',
-    EMAIL_USER      : process.env.EMAIL_USER              || 'masonmahaffey00@gmail.com',
-    EMAIL_PASSWORD  : process.env.EMAIL_PASSWORD          || 'password',
+    EMAIL_USER      : process.env.EMAIL_USER              || 'your_email',
+    EMAIL_PASSWORD  : process.env.EMAIL_PASSWORD          || 'your_password',
     APP_NAME        : process.env.APP_NAME                || 'newsfiltr.com',
     APP_URL         : process.env.APP_NAME                || 'http://localhost:4200',
     SUPPORT_EMAIL   : process.env.SUPPORT_EMAIL           || 'mason@newsfiltr.com'
